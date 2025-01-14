3a4a0552feaf3b8e308cbaff287e8f3cb5a18268
aryanvar <varmaaryan2990@gmail.com>
2025-01-15 00:40:46 +0530

    updated side bar

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+import { Button } from '@/components/ui/button';
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="relative">
+      <div className="lg:flex ">
+        <div className='lg:w-[50%] lg:border-r'>
+                  <div className="p-3 flex items-center">
+                      <Button></Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home
--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -8,25 +8,61 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-
+import {
+  DragHandleHorizontalIcon,
+  MagnifyingGlassIcon,
+} from "@radix-ui/react-icons";
+import { Button } from "@/components/ui/button";
 import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Sidebar from "./Sidebar";
 
 const Navbar = () => {
   return (
-    <div className="px-2 py-3 border-b z-50 bg-background bg-opacity-0 sticky top-0 left-0 right-0 flex justify-between items-center">
-      <div className="flex items-center gap-3">
+    <div className="px-4 py-3 border-b bg-background bg-opacity-95 sticky top-0 left-0 right-0 z-50 flex justify-between items-center shadow-sm">
+      <div className="flex items-center gap-4">
         <Sheet>
-          <SheetTrigger>Open</SheetTrigger>
-          <SheetContent side="left">
+          <SheetTrigger asChild>
+            <Button
+              variant="ghost"
+              size="icon"
+              className="rounded-full h-12 w-12 flex items-center justify-center"
+            >
+              <DragHandleHorizontalIcon className="h-6 w-6 text-primary" />
+            </Button>
+          </SheetTrigger>
+          <SheetContent
+            side="left"
+            className="w-72 border-r-0 flex flex-col justify-start"
+          >
             <SheetHeader>
-              <SheetTitle>Are you absolutely sure?</SheetTitle>
-              <SheetDescription>
-                This action cannot be undone. This will permanently delete your
-                account and remove your data from our servers.
-              </SheetDescription>
+              <SheetTitle className="text-lg font-semibold mb-4"></SheetTitle>
+              <div className="text-3xl flex justify-center items-center gap-2">
+                <Avatar className="h-10 w-10">
+                  <AvatarImage src="https://cdn.pixabay.com/photo/2021/04/30/16/47/binance-logo-6219389_1280.png" />
+                  <AvatarFallback>F</AvatarFallback>
+                </Avatar>
+                <div>
+                  <span className="font-bold text-orange-700">Fox</span>
+                  <span className="font-bold">Trade</span>
+                </div>
+              </div>
             </SheetHeader>
+            <Sidebar />
           </SheetContent>
         </Sheet>
+        <p className="text-sm lg:text-base cursor-pointer">Fox Treading</p>
+        <div className="p-0 ml-9">
+          <Button variant="outline" className="flex items-center gap-3">
+            <MagnifyingGlassIcon />
+            <span>Search</span>
+          </Button>
+        </div>
+      </div>
+      <div>
+        <Avatar>
+          <AvatarFallback>F</AvatarFallback>
+        </Avatar>
       </div>
     </div>
   );
